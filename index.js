ffbd0fc4667907a2c5d2e4aee9f52e1fb5825708
ecinematic <ecinematic@yahoo.com>
2023-03-24 14:53:47 -0500

    added test question, inquirer module, and generatedMarkdown file

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+    {
+    type: "input",
+    name: "title",
+    message: "Name of your Project."
+    }
+];
+
+function initialize() {
+    inquirer.prompt(questions).then((responses) => {
+        fs.writeFileSync("./form/README.md", generateMarkdown({responses}));
+    });
+};
+
+initialize();
+
+//function writeToFile(fileName, data) {
+//    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+//};
